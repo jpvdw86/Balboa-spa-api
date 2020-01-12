2efefc917cff3ce5b8624913de57268b919d4259
jpvdw <info@jpvdw.nl>
2020-01-12 20:27:56 +0100

    fix bug validation login

--- Api.php
+++ Api.php
@@ -31,7 +31,7 @@ class Api {
         ]);
 
         $result = $this->apiCall('/users/login', 'POST', $postFields, 'application/json');
-        if(isset($result['token']) || $result['device']['device_id']){
+        if(!isset($result['token']) || !isset($result['device']['device_id'])){
             throw new \Exception("login faild");
         }
         $this->token = $result['token'];
